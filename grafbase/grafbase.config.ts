343a3368e562a13b68f03780b495767f08131b89
hans geovani andika <hansandika70@gmail.com>
2024-03-22 14:08:59 +0700

    add project initialization

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,19 @@
+import { graph, config, connector } from '@grafbase/sdk'
+
+const g = graph.Standalone()
+
+const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
+g.datasource(pg)
+
+export default config({
+  graph: g,
+  cache: {
+    rules: [
+      {
+        types: ['Query'],
+        maxAge: 60,
+        staleWhileRevalidate: 60,
+      },
+    ],
+  },
+})
